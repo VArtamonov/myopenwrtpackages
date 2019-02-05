70745a8f9a687206492bd9d176d3f2430614fc2c
VArtamonov <avv200071@mail.ru>
2019-02-06 00:12:06 +0400

    Commit

--- git2server-init-localhost.sh
+++ git2server-init-localhost.sh
@@ -7,8 +7,10 @@ gitp1=myopenwrtpackages
 cwd=$(pwd)
 
 cd /srv/git
-echo "Create dir $gitp1.git ..."
-sudo  -u gitdaemon mkdir -p -v $gitp1.git
+if [ ! -d $gitp1.git ]; then
+	echo "Create dir $gitp1.git ..."
+	sudo  -u gitdaemon mkdir -p -v $gitp1.git
+fi
 
 cd $cwd
 pwd
@@ -20,3 +22,7 @@ git clone --bare $gitp1
 
 echo "Copy to local server ..."
 sudo  -u gitdaemon cp -rf $gitp1.git/* /srv/git/$gitp1.git
+
+cd $cwd
+rm -rdf $gitp1.git/*
+
