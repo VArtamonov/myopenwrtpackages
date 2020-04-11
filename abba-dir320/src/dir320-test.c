c7b046dd7135bf5d2884449765bbbb4c2f2eb134
VArtamonov <avv200071@mail.ru>
2020-04-11 08:42:36 +0400

    Commit

---
+++ abba-dir320/src/dir320-test.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <glob.h>
+#include <ctype.h>
+
+int main(int argc, char **argv)
+{
+	int ret = 0;
+
+//	if (argc < 2)
+//		usage(argv[0]);
+
+		fprintf(stderr, "ABBA: Test D-LINK DIR-320 !!!\n");
+
+	return ret;
+}
