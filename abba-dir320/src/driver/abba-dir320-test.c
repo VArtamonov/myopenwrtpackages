0ea4987cb1bbdb7616413812abbe4c4326629d41
VArtamonov <avv200071@mail.ru>
2020-04-14 00:37:34 +0400

    Commit

--- abba-dir320/src/driver/abba-dir320-test.c
+++ abba-dir320/src/driver/abba-dir320-test.c
@@ -4,6 +4,11 @@
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/kernel.h> /* Needed for KERN_INFO */
 
+#define DRV_NAME	"abba-dire320-test"
+#define DRV_VERSION	"0.0.1"
+#define DRV_DESC	"ABBA DIR-320 driver"
+
+
 int init_module(void)
 {
 printk(KERN_INFO "Hello world 1.\n");
@@ -17,3 +22,8 @@ void cleanup_module(void)
 {
 printk(KERN_INFO "Goodbye world 1.\n");
 }
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("ABBA 2020");
+MODULE_DESCRIPTION(DRV_DESC);
+MODULE_VERSION(DRV_VERSION);
