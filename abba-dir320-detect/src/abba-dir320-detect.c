b53333ed84bc844e522327761e53cf0e50cdcb47
VArtamonov <avv200071@mail.ru>
2020-04-18 20:53:41 +0400

    Commit

--- abba-dir320-detect/src/abba-dir320-detect.c
+++ abba-dir320-detect/src/abba-dir320-detect.c
@@ -2,7 +2,8 @@
 * hello−1.c − The simplest kernel module.
 */
 #include <linux/module.h> /* Needed by all modules */
-#include <linux/kernel.h> /* Needed for KERN_INFO */
+#include <linux/version.h>
+#include <linux/kmod.h> /* Needed for KERN_INFO */
 
 #include <linux/leds.h>
 #include <linux/input.h>
