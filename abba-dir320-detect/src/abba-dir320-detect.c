85e105b86685892ca0bd081260199048d9c0f36c
VArtamonov <avv200071@mail.ru>
2020-04-20 00:31:35 +0400

    Commit

--- abba-dir320-detect/src/abba-dir320-detect.c
+++ abba-dir320-detect/src/abba-dir320-detect.c
@@ -1,29 +1,21 @@
 /*
- *  ABBA test driver
+ *  ABBA DIR-320 driver
  *
  *  Copyright (C) 2020 VArtamonov <vartamonov@abba.org>
  *
- *  Based on the diag.c - GPIO interface driver for Broadcom boards
- *    Copyright (C) 2006 Mike Baker <mbm@openwrt.org>,
- *    Copyright (C) 2006-2007 Felix Fietkau <nbd@nbd.name>
- *    Copyright (C) 2008 Andy Boyett <agb@openwrt.org>
- *
- *  This program is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License version 2 as published
- *  by the Free Software Foundation.
  */
 
 #include <linux/module.h>
 #include <linux/version.h>
 #include <linux/kmod.h>
 
-#include <linux/leds.h>
-#include <linux/input.h>
-#include <linux/gpio_keys.h>
+//#include <linux/leds.h>
+//#include <linux/input.h>
+//#include <linux/gpio_keys.h>
 
 //#include <linux/interrupt.h>
-#include <bcm47xx_board.h>
-#include <bcm47xx.h>
+//#include <bcm47xx_board.h>
+//#include <bcm47xx.h>
 
 //#define DRV_NAME	"abba-dire320-detect"
 //#define DRV_VERSION	"0.0.1"
@@ -173,17 +165,16 @@ static int __init abba_dir320_custom_init(void)
 	return 0;
 }
 
-static void __exit abba_dir320__custom_exit(void)
+static void __exit abba_dir320_custom_exit(void)
 {
 	printk(KERN_INFO "ABBA DIR-320 Detect ... \n");
 	printk(KERN_INFO DRV_DESC " version " DRV_VERSION "\n");
 }
 
+module_init(abba_dir320_custom_init);
+module_exit(abba_dir320_custom_exit);
 
-MODULE_LICENSE("GPL");
+MODULE_LICENSE("MIT");
 //MODULE_AUTHOR("ABBA 2020");
 //MODULE_DESCRIPTION(DRV_DESC);
 //MODULE_VERSION(DRV_VERSION);
-
-module_init(abba_dir320_custom_init);
-module_exit(abba_dir320__custom_exit);
