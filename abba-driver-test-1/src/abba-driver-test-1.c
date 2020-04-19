3ac5890c71c6d71c466d415c569f8b33c6b621da
VArtamonov <avv200071@mail.ru>
2020-04-19 23:25:13 +0400

    Commit

--- abba-driver-test-1/src/abba-driver-test-1.c
+++ abba-driver-test-1/src/abba-driver-test-1.c
@@ -1,6 +1,21 @@
-#include <linux/kernel.h>
+/*
+ *  ABBA test driver
+ *
+ *  Copyright (C) 2020 VArtamonov <vartamonov@abba.org>
+ *
+ *  Based on the diag.c - GPIO interface driver for Broadcom boards
+ *    Copyright (C) 2006 Mike Baker <mbm@openwrt.org>,
+ *    Copyright (C) 2006-2007 Felix Fietkau <nbd@nbd.name>
+ *    Copyright (C) 2008 Andy Boyett <agb@openwrt.org>
+ *
+ *  This program is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License version 2 as published
+ *  by the Free Software Foundation.
+ */
+
 #include <linux/module.h>
-#include <linux/platform_device.h>
+#include <linux/version.h>
+#include <linux/kmod.h>
 
 #define DRV_NAME	"abba-driver-test-1"
 #define DRV_DESC	"ABBA - test driver"
