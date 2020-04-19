fe318180058a51a9850fb099f7b71187631a270b
VArtamonov <avv200071@mail.ru>
2020-04-20 00:19:07 +0400

    Commit

--- abba-driver-test-1/src/abba-driver-test-1.c
+++ abba-driver-test-1/src/abba-driver-test-1.c
@@ -24,8 +24,8 @@
 static int __init abba_example_init(void)
 {
 	printk(KERN_INFO "ABBA test driver ... \n");
-	printk(KERN_INFO DRV_DESC " version " DRV_VERSION "\n");
-	printk(KERN_INFO "ABBA - Hello, World!\n");
+//	printk(KERN_INFO DRV_DESC " version " DRV_VERSION "\n");
+	printk(KERN_INFO "ABBA - Hello\n");
 
 	return 0;
 }
@@ -33,7 +33,7 @@ static int __init abba_example_init(void)
 static void __exit abba_example_exit(void)
 {
 	printk(KERN_INFO "ABBA test driver ... \n");
-	printk(KERN_INFO "ABBA Goodbye, World!\n");
+	printk(KERN_INFO "ABBA - Goodbye\n");
 }
 
 module_init(abba_example_init);
