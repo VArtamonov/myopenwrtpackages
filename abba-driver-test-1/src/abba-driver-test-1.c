3352a389542ff0042040449af19afedbb2070f4e
VArtamonov <avv200071@mail.ru>
2020-04-19 10:48:18 +0400

    Commit

---
+++ abba-driver-test-1/src/abba-driver-test-1.c
@@ -0,0 +1,26 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE(“GPL”);
+MODULE_AUTHOR(“VArtamonov”);
+MODULE_DESCRIPTION(“ABBA - simple example Linux module.”);
+MODULE_VERSION(“0.01”);
+
+static int __init abba_example_init(void)
+{
+	printk(KERN_INFO "ABBA test driver ... \n");
+	printk(KERN_INFO DRV_DESC " version " DRV_VERSION "\n");
+	printk(KERN_INFO “ABBA - Hello, World!\n”);
+
+	return 0;
+}
+
+static void __exit abba_example_exit(void)
+{
+	printk(KERN_INFO "ABBA test driver ... \n");
+	printk(KERN_INFO “ABBA Goodbye, World!\n”);
+}
+
+module_init(abba_example_init);
+module_exit(abba_example_exit);
