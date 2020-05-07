e650cb7f9a92d7bc4aff5b4ac5cb49ce6911db44
VArtamonov <avv200071@mail.ru>
2020-05-07 13:33:04 +0400

    Commit at 2020.05.07 13:33:04

--- git2server-push-github.sh
+++ git2server-push-github.sh
@@ -8,8 +8,15 @@ pwd
 
 echo "FEED update mypackages ..."
 
+# вывод времени
+time_now=$(date  "+%Y.%m.%d %H:%M:%S")
+my_commit="Commit at "
+commit1=$my_commit$time_now
+echo $commit1
+
 git add .
-git commit -a -S -m 'Commit '
+git commit -a -S -m "$commit1"
+
 git push --force
 
 echo "End ..."
