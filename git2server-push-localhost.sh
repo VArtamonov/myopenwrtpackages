e4ae09f644b7ecac9c9bb09e1298b6b834574044
VArtamonov <avv200071@mail.ru>
2020-05-07 13:23:45 +0400

    Commit

--- git2server-push-localhost.sh
+++ git2server-push-localhost.sh
@@ -8,6 +8,10 @@ pwd
 
 echo "FEED update mypackages ..."
 
+# вывод времени
+time_now=$(date  "+%Y.%m.%d %H:%M:%S")
+echo $time_now
+my_commit="Commit "
 git add .
 git commit -a -S -m 'Commit '
 git push localserver
